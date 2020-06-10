b087228aa7176021a45da50c0e6d2adb6c3d380b
Alex Vassiliou <avassiliou@zendesk.com>
2020-06-10 19:07:34 +1000

    added service and create function

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/alexvassiliou/messages
+
+go 1.14
+
+require (
+	github.com/golang/protobuf v1.4.2
+	github.com/google/uuid v1.1.1
+	google.golang.org/grpc v1.29.1
+)
---
+++ service/api.go
@@ -0,0 +1,42 @@
+package api
+
+import (
+	context "context"
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
+//Server is the message server it holds a collection of messages
+type Server struct {
+	db []Message
+}
+
+//Create a message
+func (s *Server) Create(ctx context.Context, req *CreateRequest) (*CreateResponse, error) {
+	// Create the id for the new message
+	id, err := uuid.NewUUID()
+	if err != nil {
+		return nil, fmt.Errorf("Create: %s", err)
+	}
+
+	// Create the message from the request
+	m := Message{
+		Id:      id.String(),
+		Title:   req.Message.Title,
+		Content: req.Message.Content,
+		Time:    req.Message.Time,
+	}
+
+	// Add the message to the db store
+	s.db = append(s.db, m)
+
+	// Success message that the message has been added to the database
+	success := fmt.Sprintf("New entry created: %v", m.Id)
+	fmt.Println(success)
+
+	// Return the response with the message id
+	return &CreateResponse{
+		Id: m.Id,
+	}, nil
+}
